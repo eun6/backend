fda5ff807eeedcf01f4e1788fdb098592cd83384
KIM MIN WOO <79193811+minwoo1999@users.noreply.github.com>
2023-07-23 22:31:18 +0900

    :wrench: template 추가

--- README.md
+++
@@ -1,2 +0,0 @@
-# Springboot-Initializer
-spring boot 초기 코드 test
--- src/main/java/com/example/springboot/SpringbootApplication.java
+++ src/main/java/com/example/springboot/SpringbootApplication.java
@@ -1,8 +1,7 @@
 package com.example.springboot;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.context.annotation.Bean;
 @SpringBootApplication
 public class SpringbootApplication {
 
